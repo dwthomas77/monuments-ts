2459359405868a2bb9203611ef8e8dc4b85d71ea
David Thomas <david.thomas3@gmail.com>
2023-02-17 08:11:15 -0500

    testing device change and key gen

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import translations from './translations.json';
-import APP_TEST_IDS from './testIds/App.json';
+import APP_TEST_IDS from './testIds/app.json';
 
-it('renders the correct header', () => {
+it('Renders the correct header', () => {
   render(<App />);
   const headerElement = screen.getByText(translations.app.header);
   expect(headerElement).toBeInTheDocument();
